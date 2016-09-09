b7a59550c290393a92223c7572cb615d3ec3e5fa
xieqingyao <xieqingyao@zookingsoft.com>
2016-09-09 14:04:29 +0800

    完成

--- src/com/coolweather/app/util/HttpUtil.java
+++ src/com/coolweather/app/util/HttpUtil.java
@@ -1,11 +1,9 @@
 package com.coolweather.app.util;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class HttpUtil {
@@ -19,21 +17,18 @@ public class HttpUtil {
 					URL url = new URL(address);
 					connection = (HttpURLConnection) url.openConnection();
 					connection.setRequestMethod("GET");
-					connection.setReadTimeout(5000);
-					connection.setConnectTimeout(5000);
+					connection.setConnectTimeout(8000);
+					connection.setReadTimeout(8000);
 					InputStream in = connection.getInputStream();
 					BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 					StringBuilder response = new StringBuilder();
-					String line;
+					String line = "";
 					while((line = reader.readLine()) != null){
 						response.append(line);
-					}
-					if(listener != null){
 						//回调onFinish()方法
 						listener.onFiniish(response.toString());
 					}
 				} catch (Exception e) {
-					e.printStackTrace();
 					if(listener != null){
 						//回调onError()方法
 						listener.onError(e);
